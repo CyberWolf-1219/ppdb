8e2521258ca3d96ccf34974093414427cbf61c7b
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-06 16:04:15 +0530

    removed height & added font weight parameter

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,14 +10,35 @@ const TYPE_OPTIONS = {
   secondary: 'border-pallet-accent bg-transparent text-pallet-accent',
 };
 
+const TEXT_SIZE_OPTIONS = {
+  sm: 'text-sm',
+  md: 'text-base',
+  lg: 'text-lg',
+};
+
+const FONT_WEIGHT_OPTIONS = {
+  thing: '!font-thin',
+  normal: '!font-normal',
+  bold: '!font-bold',
+};
+
 interface Props {
-  width: keyof typeof WIDTH_OPTIONS;
   type: keyof typeof TYPE_OPTIONS;
+  width: keyof typeof WIDTH_OPTIONS;
+  textSize: keyof typeof TEXT_SIZE_OPTIONS;
+  fontWeight?: keyof typeof FONT_WEIGHT_OPTIONS;
   action: Function;
   children: ReactNode;
 }
 
-function Button({ type, width, action, children }: Props) {
+function Button({
+  type,
+  width,
+  textSize,
+  fontWeight = 'normal',
+  action,
+  children,
+}: Props) {
   function clickHandler() {
     action();
   }
@@ -25,7 +46,7 @@ function Button({ type, width, action, children }: Props) {
   return (
     <button
       onClick={clickHandler}
-      className={`${WIDTH_OPTIONS[width]} h-fit px-[1em] py-[0.75em] border-[2px] ${TYPE_OPTIONS[type]} font-semibold text-lg`}>
+      className={`${WIDTH_OPTIONS[width]} h-fit px-[2em] py-[0.75em] border-[2px] ${TYPE_OPTIONS[type]} font-semibold ${TEXT_SIZE_OPTIONS[textSize]} ${FONT_WEIGHT_OPTIONS[fontWeight]} rounded-sm leading-[100%]`}>
       {children}
     </button>
   );
