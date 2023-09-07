4b4d257f338e147def8e08fddf672ed5fc456dcd
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-07 11:09:53 +0530

    created modal bg

---
+++ src/components/ModalBG/ModalBG.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from 'react';
+
+function ModalBG({ children }: { children: ReactNode }) {
+  return (
+    <div
+      className={
+        'fixed z-[100] inset-[0] w-auto h-auto flex flex-col items-center justify-center bg-black/60 backdrop-blur-sm'
+      }>
+      {children}
+    </div>
+  );
+}
+
+export default ModalBG;
