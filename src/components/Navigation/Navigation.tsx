1638217b5056b86ef99868d0cbc145a92b7b1954
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-07 12:02:10 +0530

    added registration form ui

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,59 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, type ReactNode } from 'react';
 import Logo from '../Logo/Logo';
 import Button from '../Button/Button';
+import Portal from '../Portal/Portal';
+import ModalBG from '../ModalBG/ModalBG';
+import RegistrationForm from '../RegistrationForm/RegistrationForm';
 
 function Navigation() {
-  const [viewRigistrationModal, setViewRigistrationModal] = useState(false);
+  const [viewRegistrationModal, setViewRegistrationModal] = useState(false);
   const [viewLoginModal, setViewLoginModal] = useState(false);
   const [viewUploadModal, setViewUploadModal] = useState(false);
 
-  function registerButtonHandler() {}
+  function registerButtonHandler() {
+    setViewRegistrationModal((prevState) => {
+      return !prevState;
+    });
+  }
 
   function loginButtonHandler() {}
 
   function uploadButtonHandler() {}
 
   return (
-    <nav className={'w-full h-fit px-[1rem] py-[1rem] border-b-[2px]'}>
-      <div className={'flex flex-col md:flex-row items-center justify-between'}>
-        <Logo />
-        <ul
-          className={
-            'w-full md:max-w-[50%] lg:max-w-[40%] mt-[1rem] md:m-[0] flex flex-col md:flex-row items-center justify-center gap-[1rem]'
-          }>
-          <div className={'w-full h-fit flex flex-row item-center gap-[1rem]'}>
+    <>
+      {viewRegistrationModal ? (
+        <Portal>
+          <ModalBG>
+            <RegistrationForm formCloseHandler={registerButtonHandler} />
+          </ModalBG>
+        </Portal>
+      ) : null}
+
+      <nav className={'w-full h-fit px-[1rem] py-[1rem] border-b-[2px]'}>
+        <div
+          className={'flex flex-col md:flex-row items-center justify-between'}>
+          <Logo />
+          <ul
+            className={
+              'w-full md:max-w-[50%] lg:max-w-[40%] mt-[1rem] md:m-[0] flex flex-col md:flex-row items-center justify-center gap-[1rem]'
+            }>
+            <div
+              className={'w-full h-fit flex flex-row item-center gap-[1rem]'}>
+              <li className={'w-full'}>
+                <Button
+                  type={'secondary'}
+                  width={'full'}
+                  textSize={'md'}
+                  action={registerButtonHandler}>
+                  Register
+                </Button>
+              </li>
+              <li className={'w-full'}>
+                <Button
+                  type={'secondary'}
+                  width={'full'}
+                  textSize={'md'}
+                  action={loginButtonHandler}>
+                  Login
+                </Button>
+              </li>
+            </div>
             <li className={'w-full'}>
               <Button
-                type={'secondary'}
+                type={'primary'}
                 width={'full'}
                 textSize={'md'}
-                action={registerButtonHandler}>
-                Register
+                fontWeight={'bold'}
+                action={uploadButtonHandler}>
+                Upload
               </Button>
             </li>
-            <li className={'w-full'}>
-              <Button
-                type={'secondary'}
-                width={'full'}
-                textSize={'md'}
-                action={loginButtonHandler}>
-                Login
-              </Button>
-            </li>
-          </div>
-          <li className={'w-full'}>
-            <Button
-              type={'primary'}
-              width={'full'}
-              textSize={'md'}
-              fontWeight={'bold'}
-              action={uploadButtonHandler}>
-              Upload
-            </Button>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
 
