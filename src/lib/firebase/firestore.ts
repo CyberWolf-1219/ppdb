350a0a8ddc8f0b2009a90304adfef260857cb829
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-12 12:24:08 +0530

    added document search

--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -1,4 +1,4 @@
-import { getFirestore, doc, addDoc, getDoc, deleteDoc, updateDoc, collection } from 'firebase/firestore'
+import { getFirestore, doc, addDoc, getDoc, deleteDoc, updateDoc, collection, query, where, getDocs } from 'firebase/firestore'
 import { app } from "./client";
 
 const firestore = getFirestore(app)
@@ -7,20 +7,37 @@ const firestore = getFirestore(app)
 
 export class PastPaperEntry {
     private paperCollection;
-    private examYear: number;
-    private exam: string;
-    private examSubject: string;
-    private filePath: string;
+    private year: number | null;
+    private exam: string | null;
+    private subject: string | null;
+    private filePath: string | null;
 
-    constructor(year: number, exam: string, subject: string, cloudStoreFilePath: string) {
-        this.examYear = year;
+    constructor(year: number, exam: string, subject: string, cloudStoreFilePath?: string) {
+        this.year = year;
         this.exam = exam;
-        this.examSubject = subject
-        this.filePath = cloudStoreFilePath;
+        this.subject = subject
+        this.filePath = cloudStoreFilePath || "";
         this.paperCollection = collection(firestore, 'papers');
     }
 
     async save() {
-        const result = await addDoc(this.paperCollection, { year: this.examYear, exam: this.exam, subject: this.examSubject, file: this.filePath })
+        if (!this.filePath) {
+            throw new Error('[-] Cloud File Path Needed to Save the Entry!')
+        } else {
+
+            const result = await addDoc(this.paperCollection, { year: this.year, exam: this.exam, subject: this.subject, file: this.filePath })
+
+            return result;
+        }
+    }
+
+    async search() {
+        if (this.year == null || this.exam == null || this.subject == null) {
+            throw new Error('[-] Need All Parameters to Perform the Search!')
+        }
+
+        const result = await getDoc(doc(this.paperCollection, `${this.year}_${this.exam}_${this.subject}`))
+
+        return result.data();
     }
 }
