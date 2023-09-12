e5a342fec9328b63be41f69eddb40101166b6611
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-12 07:47:31 +0530

    removed unnecessary styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -47,7 +47,7 @@ function Button({
   return (
     <button
       onClick={clickHandler}
-      className={`${WIDTH_OPTIONS[width]} h-fit px-[2em] py-[0.75em] border-[2px] ${TYPE_OPTIONS[type]} font-semibold ${TEXT_SIZE_OPTIONS[textSize]} ${FONT_WEIGHT_OPTIONS[fontWeight]} rounded-sm leading-[100%]`}>
+      className={`${WIDTH_OPTIONS[width]} h-fit px-[2em] py-[0.75em] border-[2px] ${TYPE_OPTIONS[type]} ${TEXT_SIZE_OPTIONS[textSize]} ${FONT_WEIGHT_OPTIONS[fontWeight]} rounded-sm leading-[100%]`}>
       {children}
     </button>
   );
