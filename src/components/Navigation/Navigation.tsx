966bf5892fcb9e75a4ac9785cca9ae21623d158b
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-16 14:31:44 +0530

    added cookies provider

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -6,6 +6,7 @@ import ModalBG from '../ModalBG/ModalBG';
 import RegistrationForm from '../RegistrationForm/RegistrationForm';
 import LoginForm from '../LoginForm/LoginForm';
 import UploadForm from '../UploadForm/UploadForm';
+import { CookiesProvider } from 'react-cookie';
 
 function Navigation() {
   const [viewRegistrationModal, setViewRegistrationModal] = useState(false);
@@ -31,71 +32,75 @@ function Navigation() {
   }
 
   return (
-    <>
-      {viewRegistrationModal ? (
-        <Portal>
-          <ModalBG>
-            <RegistrationForm formCloseHandler={registerButtonHandler} />
-          </ModalBG>
-        </Portal>
-      ) : null}
-      {viewLoginModal ? (
-        <Portal>
-          <ModalBG>
-            <LoginForm formCloseHandler={loginButtonHandler} />
-          </ModalBG>
-        </Portal>
-      ) : null}
-      {viewUploadModal ? (
-        <Portal>
-          <ModalBG>
-            <UploadForm formCloseHandler={uploadButtonHandler} />
-          </ModalBG>
-        </Portal>
-      ) : null}
-      <nav className={'w-full h-fit px-[1rem] py-[1rem] border-b-[2px]'}>
-        <div
-          className={'flex flex-col md:flex-row items-center justify-between'}>
-          <Logo />
-          <ul
+    <CookiesProvider>
+      <>
+        {viewRegistrationModal ? (
+          <Portal>
+            <ModalBG>
+              <RegistrationForm formCloseHandler={registerButtonHandler} />
+            </ModalBG>
+          </Portal>
+        ) : null}
+        {viewLoginModal ? (
+          <Portal>
+            <ModalBG>
+              <LoginForm formCloseHandler={loginButtonHandler} />
+            </ModalBG>
+          </Portal>
+        ) : null}
+        {viewUploadModal ? (
+          <Portal>
+            <ModalBG>
+              <UploadForm formCloseHandler={uploadButtonHandler} />
+            </ModalBG>
+          </Portal>
+        ) : null}
+        <nav className={'w-full h-fit px-[1rem] py-[1rem] border-b-[2px]'}>
+          <div
             className={
-              'w-full md:max-w-[60%] lg:max-w-[40%] mt-[1rem] md:m-[0] flex flex-col md:flex-row items-center justify-center gap-[1rem]'
+              'flex flex-col md:flex-row items-center justify-between'
             }>
-            <div
-              className={'w-full h-fit flex flex-row item-center gap-[1rem]'}>
+            <Logo />
+            <ul
+              className={
+                'w-full md:max-w-[60%] lg:max-w-[40%] mt-[1rem] md:m-[0] flex flex-col md:flex-row items-center justify-center gap-[1rem]'
+              }>
+              <div
+                className={'w-full h-fit flex flex-row item-center gap-[1rem]'}>
+                <li className={'w-full'}>
+                  <Button
+                    type={'secondary'}
+                    width={'full'}
+                    textSize={'md'}
+                    action={registerButtonHandler}>
+                    Register
+                  </Button>
+                </li>
+                <li className={'w-full'}>
+                  <Button
+                    type={'secondary'}
+                    width={'full'}
+                    textSize={'md'}
+                    action={loginButtonHandler}>
+                    Login
+                  </Button>
+                </li>
+              </div>
               <li className={'w-full'}>
                 <Button
-                  type={'secondary'}
+                  type={'primary'}
                   width={'full'}
                   textSize={'md'}
-                  action={registerButtonHandler}>
-                  Register
+                  fontWeight={'bold'}
+                  action={uploadButtonHandler}>
+                  Upload
                 </Button>
               </li>
-              <li className={'w-full'}>
-                <Button
-                  type={'secondary'}
-                  width={'full'}
-                  textSize={'md'}
-                  action={loginButtonHandler}>
-                  Login
-                </Button>
-              </li>
-            </div>
-            <li className={'w-full'}>
-              <Button
-                type={'primary'}
-                width={'full'}
-                textSize={'md'}
-                fontWeight={'bold'}
-                action={uploadButtonHandler}>
-                Upload
-              </Button>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
+            </ul>
+          </div>
+        </nav>
+      </>
+    </CookiesProvider>
   );
 }
 
