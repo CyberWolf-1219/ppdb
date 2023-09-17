9fa4c79a1db020f4da0709a014a9ab6bcd64e624
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-17 14:57:34 +0530

    uploader email display

--- src/components/SearchResultCard/SearchResultCard.tsx
+++ src/components/SearchResultCard/SearchResultCard.tsx
@@ -1,21 +1,22 @@
-import Button from '../Button/Button';
+import type { UIEvent } from 'react';
 
 interface Props {
+  uploaderEmail: string;
   imageLink: string;
   downloadLink: string;
 }
 
-function SearchResultCard({ imageLink, downloadLink }: Props) {
+function SearchResultCard({ uploaderEmail, imageLink, downloadLink }: Props) {
   return (
     <article
       className={
-        'w-full h-fit shadow-md shadow-black/30 bg-pallet-light rounded-sm overflow-hidden'
+        'w-full h-fit shadow-md shadow-black/30 bg-pallet-light rounded-[0.25rem] overflow-hidden'
       }>
       <img
         src={imageLink}
         alt='Exam paper preview image'
         className={
-          'aspect-[3/3.5] w-full h-auto object-contain bg-black/20 text-center'
+          'aspect-[3/3.5] w-full h-auto object-cover bg-black/20 text-center'
         }
       />
       <div className={'w-full h-fit'}>
@@ -23,16 +24,18 @@ function SearchResultCard({ imageLink, downloadLink }: Props) {
           className={
             'mt-[1rem] text-base font-bold text-center text-pallet-dark'
           }>
-          Year/Exam/Subject
+          {uploaderEmail}
         </p>
-        <Button
-          type={'primary'}
-          width={'full'}
-          height={'fit'}
-          textSize={'md'}
-          action={() => {}}>
-          Download
-        </Button>
+
+        <a
+          href={downloadLink}
+          target='_blank'
+          rel='noopener noreferrer'
+          className={
+            'inline-block w-full h-fit px-[1rem] py-[1rem] font-bold bg-pallet-accent text-white text-center leading-[100%]'
+          }>
+          DOWNLOAD
+        </a>
       </div>
     </article>
   );
