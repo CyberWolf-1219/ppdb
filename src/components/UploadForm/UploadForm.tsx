b43b1fee5d015d789d15cade33cfaa9a3777a06e
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-17 14:46:52 +0530

    changed from xhr to fetch

--- src/components/UploadForm/UploadForm.tsx
+++ src/components/UploadForm/UploadForm.tsx
@@ -1,4 +1,10 @@
-import React, { useRef, useState, type FormEvent, type UIEvent } from 'react';
+import React, {
+  useRef,
+  useState,
+  type FormEvent,
+  type UIEvent,
+  useCallback,
+} from 'react';
 import Button from '../Button/Button';
 import { useCookies } from 'react-cookie';
 
@@ -16,38 +22,27 @@ function UploadForm({ formCloseHandler }: Props) {
 
   const [isSending, setIsSending] = useState(false);
 
-  function submitHandler(e: FormEvent) {
+  const upload = useCallback(async (e: FormEvent) => {
+    setIsSending(true);
+
     e.preventDefault();
     const fd = new FormData(form.current!);
-    const formdata = fd.entries();
-    console.log(formdata);
 
-    const XHR = new XMLHttpRequest();
-    XHR.open('POST', '/api/upload');
+    const response = await fetch('/api/upload', { method: 'POST', body: fd });
+    console.log(await response.json());
 
-    XHR.addEventListener('loadstart', (e) => {
-      setIsSending(true);
-    });
-
-    XHR.addEventListener('load', (e) => {
-      setIsSending(false);
-    });
-
-    XHR.addEventListener('error', (e) => {
-      alert(e);
-      console.log(e);
-    });
-
-    console.log(XHR);
-    XHR.send(fd);
-  }
+    setIsSending(false);
+  }, []);
 
   if (!cookies['logged-in'] || cookies['logged-in'] !== 1) {
     return (
-      <div className={'w-fit h-fit p-[1rem] rounded-sm bg-white border-[2px]'}>
+      <div
+        className={
+          'w-fit h-fit p-[1rem] rounded-[0.25rem] bg-white border-[2px]'
+        }>
         <p
           className={
-            'w-full h-fit mt-[2rem] mb-[1rem] text-3xl font-bold text-center'
+            'w-full h-fit mt-[2rem] mb-[0.5rem] text-3xl font-bold text-center'
           }>
           OOPs
         </p>
@@ -75,7 +70,10 @@ function UploadForm({ formCloseHandler }: Props) {
 
   if (isSending) {
     return (
-      <div className={'w-fit h-fit p-[1rem] rounded-sm bg-white border-[2px]'}>
+      <div
+        className={
+          'w-fit h-fit p-[1rem] rounded-[0.25rem] bg-white border-[2px]'
+        }>
         <img
           src='/upload_animation.gif'
           alt='upload animation'
@@ -87,15 +85,17 @@ function UploadForm({ formCloseHandler }: Props) {
   return (
     <form
       ref={form}
-      onSubmit={submitHandler}
+      onSubmit={upload}
       action=''
-      className={'w-fit h-fit p-[1rem] bg-white rounded-sm'}>
-      <p className={'mt-[2rem] mb-[1rem] text-center text-3xl font-bold'}>
+      className={
+        'w-fit h-full p-[1rem] bg-white rounded-[0.25rem] overflow-y-auto'
+      }>
+      <p className={'mt-[1rem] mb-[0.5rem] text-center text-3xl font-bold'}>
         Upload an Exam Paper
       </p>
       <fieldset
         className={
-          'w-fit h-fit p-[1rem] flex flex-col items-start justify-start border-[2px] rounded-sm'
+          'w-fit h-fit p-[1rem] flex flex-col items-start justify-start border-[2px] rounded-[0.25rem]'
         }>
         <legend>Enter Paper Details</legend>
         <input
@@ -103,49 +103,63 @@ function UploadForm({ formCloseHandler }: Props) {
           name='email'
           value={cookies['user-email']}
         />
-        <label htmlFor='input_exam-year'>Exam Year: </label>
+        <label
+          htmlFor='input_exam-year'
+          className={'text-sky-900/70 font-semibold'}>
+          Exam Year:{' '}
+        </label>
         <input
           type='number'
           name='year'
           id='input_exam-year'
           min={2000}
+          max={new Date().getFullYear()}
+          placeholder={'Ex: 2000'}
           className={
-            'w-full h-fit mb-[1rem] px-[0.5em] py-[0.25em] text-[1.25rem] leading-[100%] border-[2px] rounded-sm'
+            'w-full h-fit mb-[0.5rem] px-[0.5rem] pt-[0.4rem] pb-[0.25rem] text-[1.25rem] leading-[100%] border-[2px] rounded-[0.25rem]'
           }
           required={true}
         />
 
-        <label htmlFor='select_exam'>Select the Exam: </label>
+        <label
+          htmlFor='select_exam'
+          className={'text-sky-900/70 font-semibold'}>
+          Select the Exam:{' '}
+        </label>
         <select
           name='exam'
           id='select_exam'
           defaultValue={'dummy'}
           className={
-            'w-full h-fit mb-[1rem] px-[0.5em] py-[0.25em] text-[1.25rem] leading-[100%] border-[2px] rounded-sm'
+            'w-full h-fit mb-[0.5rem] px-[0.5rem] pt-[0.4rem] pb-[0.25rem] text-[1.25rem] leading-[100%] border-[2px] rounded-[0.25rem]'
           }
           required={true}>
           <option
             value='dummy'
             disabled={true}>
-            SELECT AN EXAM
+            Select An Exam
           </option>
           <option value='5'>Grade 5</option>
           <option value='ol'>GCE O/L</option>
           <option value='al'>GCE A/L</option>
         </select>
 
-        <label htmlFor='select_subject'>Select the Subject: </label>
+        <label
+          htmlFor='select_subject'
+          className={'text-sky-900/70 font-semibold'}>
+          Select the Subject:{' '}
+        </label>
         <select
           name='subject'
           id='select_subject'
           defaultValue={'dummy'}
           className={
-            'w-full h-fit mb-[1rem] px-[0.5em] py-[0.25em] text-[1.25rem] leading-[100%] border-[2px] rounded-sm'
+            'w-full h-fit mb-[0.5rem] px-[0.5em] pt-[0.4rem] pb-[0.25rem] text-[1.25rem] leading-[100%] border-[2px] rounded-[0.25rem]'
           }>
           <option
             value='dummy'
             disabled={true}>
-            SELECT A SUBJECT
+            Select A Subject
           </option>
           <option value='english'>English</option>
           <option value='sinhala'>Sinhala</option>
@@ -154,7 +168,9 @@ function UploadForm({ formCloseHandler }: Props) {
           <option value='science'>Science</option>
         </select>
 
-        <label htmlFor='file_exam-paper-screenshot'>
+        <label
+          htmlFor='file_exam-paper-screenshot'
+          className={'text-sky-900/70 font-semibold'}>
           Select A Screenshot of the File:
         </label>
         <input
@@ -163,29 +179,34 @@ function UploadForm({ formCloseHandler }: Props) {
           id='file_exam-paper-screenshot'
           accept={'.jpg, .jpeg, .png'}
           className={
-            'w-full h-fit mb-[1rem] px-[0.5em] py-[0.25em] text-[1.25em] leading-[100%] border-[2px] rounded-sm'
+            'w-full h-fit mb-[0.5rem] px-[0.25rem] py-[0.25rem] text-[1.25em] leading-[100%] border-[2px] rounded-[0.25rem]'
           }
           required={true}
         />
-        <label htmlFor='file_exam-paper'>Select the Document:</label>
+        <label
+          htmlFor='file_exam-paper'
+          className={'text-sky-900/70 font-semibold'}>
+          Select the Document:
+        </label>
         <input
           type='file'
           name='file'
           id='file_exam-paper'
           accept={'.pdf'}
           className={
-            'w-full h-fit mb-[1rem] px-[0.5em] py-[0.25em] text-[1.25em] leading-[100%] border-[2px] rounded-sm'
+            'w-full h-fit mb-[0.5rem] px-[0.25rem] py-[0.25rem] text-[1.25em] leading-[100%] border-[2px] rounded-[0.25rem]'
           }
           required={true}
         />
-        <br />
-        <div className={'w-full h-fit flex flex-col gap-[0.25rem]'}>
-          <button
-            className={
-              'px-[2em] py-[0.75em] border-[2px] border-transparent bg-pallet-accent text-pallet-light text-lg font-bold rounded-sm leading-[100%]'
-            }>
+        <div className={'w-full h-fit mt-[1rem] flex flex-col gap-[0.25rem]'}>
+          <Button
+            type={'primary'}
+            textSize={'lg'}
+            width={'full'}
+            fontWeight={'bold'}
+            action={() => {}}>
             Upload the Document
-          </button>
+          </Button>
           <Button
             type={'secondary'}
             textSize={'lg'}
