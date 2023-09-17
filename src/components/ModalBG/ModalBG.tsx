9a24f2dba7c3ca938bf5d26809e10b3c7aaca8d9
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-17 14:46:02 +0530

    add padding

--- src/components/ModalBG/ModalBG.tsx
+++ src/components/ModalBG/ModalBG.tsx
@@ -4,7 +4,7 @@ function ModalBG({ children }: { children: ReactNode }) {
   return (
     <div
       className={
-        'fixed z-[100] inset-[0] w-auto h-auto flex flex-col items-center justify-center bg-black/60 backdrop-blur-sm'
+        'fixed z-[100] inset-[0] w-auto h-auto p-[1rem] flex flex-col items-center justify-center bg-black/60 backdrop-blur-sm'
       }>
       {children}
     </div>
