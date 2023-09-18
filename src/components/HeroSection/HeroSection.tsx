5369490c48972445f2d7db5cf586e2b3f45e2077
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-18 12:03:15 +0530

    added functionality to hero button

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,53 +1,73 @@
-import React from 'react';
+import React, { useCallback, useState, type UIEvent } from 'react';
 import Button from '../Button/Button';
+import Portal from '../Portal/Portal';
+import ModalBG from '../ModalBG/ModalBG';
+import UploadForm from '../UploadForm/UploadForm';
 
 function HeroSection() {
+  const [showUploadForm, setShowUploadForm] = useState(false);
+
+  const toggleUploadForm = useCallback((e: UIEvent) => {
+    e.preventDefault();
+    setShowUploadForm((prevState) => !prevState);
+  }, []);
+
   return (
-    <section
-      id='hero'
-      className={'relative z-[0]'}>
-      <div
-        className={
-          'flex flex-col md:flex-row items-center justify-start gap-[2rem]'
-        }>
-        <picture className={'relative z-[2] flex-1'}>
-          <source
-            media='(min-width:1024px)'
-            srcSet='home_hero_image/home_hero_image@3x.png'
-          />
-          <source
-            media='(min-width:768px)'
-            srcSet='home_hero_image/home_hero_image@2x.png'
-          />
-          <source
-            media='(min-width:320px)'
-            srcSet='home_hero_image/home_hero_image@1x.png'
-          />
-          <img
-            src='home_hero_image/home_hero_image@3x.png'
-            alt=''
-            width={680}
-            height={680}
-          />
-        </picture>
-        <div className={'flex-1'}>
-          <h1>School PastPaperDB</h1>
-          <p className={'text-[1rem]'}>
-            Sri Lanka's biggest school past paper database. Download any past
-            paper you want for free. Upload the missing past papers and help
-            others succeed in their education.
-          </p>
-          <Button
-            type={'primary'}
-            width={'fit'}
-            height={'fit'}
-            textSize={'md'}
-            action={() => {}}>
-            Upload
-          </Button>
+    <>
+      {showUploadForm ? (
+        <Portal>
+          <ModalBG>
+            <UploadForm formCloseHandler={toggleUploadForm} />
+          </ModalBG>
+        </Portal>
+      ) : null}
+      <section
+        id='hero'
+        className={'relative z-[0]'}>
+        <div
+          className={
+            'flex flex-col md:flex-row items-center justify-start gap-[2rem]'
+          }>
+          <picture className={'relative z-[2] flex-1'}>
+            <source
+              media='(min-width:1024px)'
+              srcSet='home_hero_image/home_hero_image@3x.png'
+            />
+            <source
+              media='(min-width:768px)'
+              srcSet='home_hero_image/home_hero_image@2x.png'
+            />
+            <source
+              media='(min-width:320px)'
+              srcSet='home_hero_image/home_hero_image@1x.png'
+            />
+            <img
+              src='home_hero_image/home_hero_image@3x.png'
+              alt=''
+              width={680}
+              height={680}
+            />
+          </picture>
+          <div className={'flex-1'}>
+            <h1>School PastPaperDB</h1>
+            <p className={'text-[1rem]'}>
+              Sri Lanka's biggest school past paper database. Download any past
+              paper you want for free. Upload the missing past papers and help
+              others succeed in their education.
+            </p>
+            <Button
+              type={'primary'}
+              width={'fit'}
+              textSize={'md'}
+              fontWeight={'bold'}
+              roundness={'full'}
+              action={toggleUploadForm}>
+              Upload
+            </Button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
